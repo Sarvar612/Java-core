e57e2d44b872533affacb7038efd0df62bfa12da
Sarvar <138777274+Sarvar612@users.noreply.github.com>
2025-10-14 16:00:36 +0500

    Delete Task2 UzumT directory

--- Task2 UzumT/src/Person.java
+++
@@ -1,9 +0,0 @@
-public class Person {
-    String name;
-    int age;
-
-    public void introduce() {
-        System.out.println("Hi, my name is " + name + " and I’m " + age + " years old.");
-    }
-}
-
